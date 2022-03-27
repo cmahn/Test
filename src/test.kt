f7138c6dd23116daa22ed2d635d0983006c1c979
cmahn <cmahn@yonsei.ac.kr>
2022-03-28 01:34:36 +0900

    20220328 문자열 등

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,34 @@
 fun main() {
     println("hello")
+    // 특수한 Array
+    val x: IntArray = IntArray(3)
+    println(x)
+    x[0] = 7
+
+//    println(x.get(0))
+//    println(x.get(1))
+//    println(x[0])
+//    println(x.size)
+
+    // 문자열
+    var xx:String = "Kotlin"
+    println(xx.get(0))
+    println(xx[0])
+    println(xx.length)
+
+    for(c in xx){
+        print(c)
+    }
+    println()
+
+
+    val s = "hello, world:\nㅋㅋㅋ"
+    println(s)
+    val ss = """
+        "' 이것은 코틀린의 
+        row string 
+        입니다'"
+        """
+    println(ss)
 }
+
